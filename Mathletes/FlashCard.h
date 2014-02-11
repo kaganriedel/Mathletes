bc413c1677336a860e6a6284c2bdbfab38f588a6
mjvoracek <mjvoracek@gmail.com>
2014-02-11 12:15:57 -0600

    building flashcards and difficulty

---
+++ Mathletes/FlashCard.h
@@ -0,0 +1,13 @@
+//
+//  FlashCard.h
+//  Mathletes
+//
+//  Created by Matthew Voracek on 2/11/14.
+//  Copyright (c) 2014 Kagan Riedel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FlashCard : NSObject
+
+@end
