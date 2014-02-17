6d4a86b0bff6312588be40b1086172f669fe27d9
Kagan Riedel <kaganriedel@gmail.com>
2014-02-17 10:39:47 -0600

    added custom tableviewcell

---
+++ Mathletes/TradeWallCell.m
@@ -0,0 +1,29 @@
+//
+//  TradeWallCell.m
+//  Mathletes
+//
+//  Created by Kagan Riedel on 2/17/14.
+//  Copyright (c) 2014 Kagan Riedel. All rights reserved.
+//
+
+#import "TradeWallCell.h"
+
+@implementation TradeWallCell
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier
+{
+    self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated
+{
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
