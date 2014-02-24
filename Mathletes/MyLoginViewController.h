b6d1c2485af866023cdad73441bff3eb2a08bc5e
Sonam Mehta <sonam2006@gmail.com>
2014-02-24 08:31:21 -0600

    committed 3

---
+++ Mathletes/MyLoginViewController.h
@@ -0,0 +1,14 @@
+//
+//  MyLoginViewController.h
+//  Mathletes
+//
+//  Created by Sonam Mehta on 2/24/14.
+//  Copyright (c) 2014 Kagan Riedel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "Parse/Parse.h"
+
+@interface MyLoginViewController : UIViewController
+
+@end
