1c7eda08c41b335de7e7d4311ba6cedde64efd7a
Sonam Mehta <sonam2006@gmail.com>
2014-02-27 12:37:01 -0600

    changed to mobilemath.co

--- Mathletes/main.m
+++ Mathletes/main.m
@@ -2,8 +2,8 @@
 //  main.m
 //  Mathletes
 //
-//  Created by Kagan Riedel on 2/7/14.
-//  Copyright (c) 2014 Kagan Riedel. All rights reserved.
+//  Created by MobileMath.co on 2/7/14.
+//  Copyright (c) 2014 MobileMath.co. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
